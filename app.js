248ab5cde8a9c148eb19f96705c4ab79294e9e69
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-15 17:35:10 +1100

    Templating Engine - EJS
    Downloaded EJS to be used as our tempting engine. We write *.ejs html
    files and configured express to ‘render’ these pages as *.html files
    when a request is made.

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.render('index', {title: 'Hello from render', list: ['a', 'b']});
 });
 
 app.get('/books', function (req, res) {
