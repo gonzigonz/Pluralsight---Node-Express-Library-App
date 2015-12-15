1c1681e0cd5df23fe098569ce452d5e1c7a7c745
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-15 16:35:17 +1100

    Setting Up Gulp - Nodemon
    Automatically detects changes and runs Gulp tasks…

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -35,4 +36,20 @@ gulp.task('inject', function () {
         .pipe(widedep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function (en) {
+            console.log('Restarting...');
+        });
 });
