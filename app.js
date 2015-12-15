39c41d8030807f4d96073675afa8758e54dcad7e
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-15 14:40:18 +1100

    Setting Up Gulp - JSHint & JSCS

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function (req, res ) {
+app.get('/', function (req, res) {
     res.send('hello world');
 });
 
-app.get('/books', function (req, res ) {
+app.get('/books', function (req, res) {
     res.send('hello books');
 });
 
