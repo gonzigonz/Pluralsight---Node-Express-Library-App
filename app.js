d654e08de93c8b725320dec3b4bfb39a1cc26783
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-15 12:56:02 +1100

    Setting Up Express - Serving Static Files
    - Configured NPM Start
    - Setup simple routing
    - Add simple bootstrap theme
    - Setup express static routing

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function (req, res ) {
+    res.send('hello world');
+});
+
+app.get('/books', function (req, res ) {
+    res.send('hello books');
+});
+
+app.listen(port, function (err) {
+    console.log('Running server on port ' + port);
+});
