ee322a9feab68e33f0e4cdfc2e09c95cf2a88be5
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-15 18:49:02 +1100

    Routing - Navigation & Books Router
    Replaced our navigation list with a ‘nav’ object that contains a link
    and text. And added a ‘/Books’ express router.

--- app.js
+++ app.js
@@ -3,14 +3,36 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.send('Books page');
+    });
+
+bookRouter.route('/Single')
+    .get(function (req, res) {
+        res.send('Single Books page');
+    });
+
+app.use('/Books', bookRouter);
+
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Hello from render', list: ['a', 'b']});
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            link: '/Books',
+            text: 'Books'
+        }, {
+            link: '/Authors',
+            text: 'Authors'
+        }]
+    });
 });
 
 app.get('/books', function (req, res) {
