d6c3e6c1d92203870b7df2ac32f8bda3a5554f17
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-20 11:21:30 +1100

    Routing - Rendering
    Updated books route, added some sample data (json) and added books view

--- app.js
+++ app.js
@@ -10,9 +10,64 @@ app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+var books = [
+    {
+        'title': 'Pride and Prejudice',
+        'genre': 'Fiction',
+        'author': 'Jane Austen',
+        'read': false
+    },
+    {
+        'title': 'War and Peace',
+        'genre': 'Fiction',
+        'author': 'Leo Tolstoy',
+        'read': false
+    },
+    {
+        'title': 'Hamlet',
+        'genre': 'Fiction',
+        'author': 'William Shakespeare',
+        'read': true
+    },
+    {
+        'title': 'Moby Dick',
+        'genre': 'Fiction',
+        'author': 'Herman Melville',
+        'read': true
+    },
+    {
+        'title': 'The Great Gatsby',
+        'genre': 'Fiction',
+        'author': 'F. Scott Fitzgerald',
+        'read': true
+    },
+    {
+        'title': 'The Adventures of Huckleberry',
+        'genre': 'Fiction',
+        'author': 'Mark Twain',
+        'read': true
+    },
+    {
+        'title': 'Alice"s Adventures in Wonderland',
+        'genre': 'Fiction',
+        'author': 'Lewis Carroll',
+        'read': false
+    }
+    ];
+
 bookRouter.route('/')
     .get(function (req, res) {
-        res.send('Books page');
+        res.render('books', {
+            title: 'Hello from render',
+            nav: [{
+                link: '/Books',
+                text: 'Books'
+            }, {
+                link: '/Authors',
+                text: 'Authors'
+            }],
+            books: books
+        });
     });
 
 bookRouter.route('/Single')
