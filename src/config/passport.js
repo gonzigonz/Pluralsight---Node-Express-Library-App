f248ac836c9cb4315cc09255cc685f72e3fbea16
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-22 15:55:42 +1100

    Authentication - Fixed passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,4 +5,13 @@ module.exports = function (app) {
     app.use(passport.initialize());
     app.use(passport.session());
 
+    passport.serializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    require('./strategies/local.strategy')();
 };
