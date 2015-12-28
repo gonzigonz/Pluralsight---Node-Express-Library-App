0ac0071ced3ca945b11a35883c15a18a7058908b
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-28 13:10:02 +1100

    Structure and 3rd Party APIs - Controllers

---
+++ src/controllers/bookController.js
@@ -0,0 +1,64 @@
+var sql = require('mssql');
+
+var bookController = function (bookService, nav) {
+
+    var middleware = function (req, res, next) {
+        //if (!req.user) {
+        // res.redirect('/');
+        //}
+        next();
+    };
+
+    var getIndex = function (req, res) {
+
+        var request = new sql.Request();
+        request.query(
+            'SELECT * FROM BOOKS',
+            function (err, recordset) {
+                res.render('bookListView', {
+                    title: 'Books',
+                    nav: nav,
+                    books: recordset
+                });
+            });
+    };
+
+    var allById = function (req, res, next) {
+        var id = req.params.id;
+        var preparedQuery = new sql.PreparedStatement();
+
+        preparedQuery.input('Id', sql.Int);
+        preparedQuery.prepare(
+            'SELECT * FROM BOOKS WHERE ID = @Id',
+            function (err) {
+                preparedQuery.execute({
+                        Id: id
+                    },
+                    function (err, recordset) {
+                        if (recordset.length === 0) {
+                            res.status(404).send('Not Found');
+                        } else {
+                            req.book = recordset[0];
+                            next();
+                        }
+                    });
+            });
+    };
+
+    var getById = function (req, res) {
+        res.render('bookView', {
+            title: 'Book',
+            nav: nav,
+            book: req.book
+        });
+    };
+
+    return {
+        middleware: middleware,
+        getIndex: getIndex,
+        allById: allById,
+        getById: getById
+    };
+};
+
+module.exports = bookController;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,61 +1,18 @@
 var express = require('express');
-var sql = require('mssql');
-
 var bookRouter = express.Router();
 
 var router = function (nav) {
 
-    bookRouter.use(function (req, res, next) {
-        if (!req.user) {
-            res.redirect('/');
-        }
-        next();
-    });
+    var bookController = require('../controllers/bookController')(null, nav);
+
+    bookRouter.use(bookController.middleware);
 
     bookRouter.route('/')
-        .get(function (req, res) {
-
-            var request = new sql.Request();
-            request.query(
-                'SELECT * FROM BOOKS',
-                function (err, recordset) {
-                    res.render('bookListView', {
-                        title: 'Books',
-                        nav: nav,
-                        books: recordset
-                    });
-                });
-        });
+        .get(bookController.getIndex);
 
     bookRouter.route('/:id')
-        .all(function (req, res, next) {
-            var id = req.params.id;
-            var preparedQuery = new sql.PreparedStatement();
-
-            preparedQuery.input('Id', sql.Int);
-            preparedQuery.prepare(
-                'SELECT * FROM BOOKS WHERE ID = @Id',
-                function (err) {
-                    preparedQuery.execute({
-                            Id: id
-                        },
-                        function (err, recordset) {
-                            if (recordset.length === 0) {
-                                res.status(404).send('Not Found');
-                            } else {
-                                req.book = recordset[0];
-                                next();
-                            }
-                        });
-                });
-        })
-        .get(function (req, res) {
-            res.render('bookView', {
-                title: 'Book',
-                nav: nav,
-                book: req.book
-            });
-        });
+        .all(bookController.allById)
+        .get(bookController.getById);
     return bookRouter;
 
 };
