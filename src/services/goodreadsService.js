efc015fde7f282d81c945aa1b441a357c46d6dfc
gonzigonz <gonzigonz@users.noreply.github.com>
2015-12-28 14:39:27 +1100

    Structure and 3rd Party APIs - HTTP

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,9 +1,36 @@
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser({
+    explicitArray: false
+});
+
 var goodreadsService = function () {
 
     var getBookById = function (id, cb) {
-        cb(null, {
-            description: 'Our Description'
-        });
+
+        var options = {
+            host: 'www.goodreads.com',
+            path: '/book/show/656?format=xml&key=R7f2jvNECCwllEs1VQ'
+            // www.goodreads.com/book/show/656?format=xml&key=R7f2jvNECCwllEs1VQ
+        };
+
+        var callback = function (response) {
+            var str = '';
+
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+
+            response.on('end', function () {
+                console.log(str);
+                parser.parseString(str,
+                    function (err, result) {
+                        cb(null, result.GoodreadsResponse.book);
+                    });
+            });
+        };
+
+        http.request(options, callback).end();
     };
 
     return {
